e4a75983d2af93df5ca17f1951b9b8edf71ee9c6
= <=>
2025-02-22 12:36:46 +0530

    add output vpc_id

--- lesson-08/part-B/outputs.tf
+++ lesson-08/part-B/outputs.tf
@@ -2,3 +2,9 @@ output "ec2_instance_private_ips" {
   description = "Private IP addresses of the EC2 instances"
   value       = module.ec2_instances.*.private_ip
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+  
+}
