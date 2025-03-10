aa38314928367f6a90eef8a7d35b69889b38ada5
= <=>
2025-03-10 19:32:17 +0530

    lifecycle

--- lesson-12/lifecycle/main.tf
+++ lesson-12/lifecycle/main.tf
@@ -24,6 +24,9 @@ resource "aws_iam_user" "accounts_2" {
   # Add a lifecycle sub-block here with the prevent_destroy meta-argument set to true.
   # Apply the users, then attempt to destroy them and analyze the error.
 
+  lifecycle {
+    create_before_destroy = false
+  }
 
 }
 
