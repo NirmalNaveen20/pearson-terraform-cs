81c2f5aa4bcb103f5ec44f0aaa2f91f81539c1ab
= <=>
2025-03-12 10:59:45 +0530

    locals

--- lesson-13/locals/main.tf
+++ lesson-13/locals/main.tf
@@ -15,14 +15,33 @@ provider "aws" {
 
 ## Build an IAM user account resource here with a for_each meta-argument referecing a locals block.
 
+resource "aws_iam_user" "accounts_marketing" {
+  for_each = local.accounts
+  name = each.value
+
+  tags = local.common_tags
+}
 
 
 ## Then build a locals block with a toset function listing 4 users.
-
+locals {
+  accounts = toset([
+    "Adam",
+    "John",
+    "Bryan",
+    "Jeffery"
+  ])
+}
 
 
 ## Finally, build a locals block with common tags and reference it from the IAM user resource.
-
+locals {
+  common_tags = {
+    Environment = "Development"
+    Department  = "Marketing"
+    Time_Creatd = timestamp()
+  }
+}
 
 
 ## More information on Locals: https://developer.hashicorp.com/terraform/language/values/locals
